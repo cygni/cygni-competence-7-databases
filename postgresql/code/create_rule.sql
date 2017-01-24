8c4797a7413ce55911a286f01780f9ace919a04c
Rikard Lindström <rikard.lindstrom@maxm.se>
2017-01-24 10:37:42 +0100

    removed unused sqls

--- postgresql/code/create_rule.sql
+++
@@ -1,6 +0,0 @@
-CREATE RULE update_holidays AS ON UPDATE TO holidays DO INSTEAD
-  UPDATE events
-  SET title = NEW.name,
-      starts = NEW.date,
-      colors = NEW.colors
-  WHERE title = OLD.name;
--- postgresql/code/holiday_view_1.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW holidays AS
-  SELECT event_id AS holiday_id, title AS name, starts AS date
-  FROM events
-  WHERE title LIKE '%Day%' AND venue_id IS NULL;
