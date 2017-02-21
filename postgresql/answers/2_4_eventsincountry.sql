9f053d9ef36b38278e4ac5fa44f4d01b11371c98
Rikard Lindström <rikard.lindstrom@maxm.se>
2017-02-21 19:01:41 +0100

    fixed 2.4

--- postgresql/answers/2_4_eventsincountry.sql
+++ postgresql/answers/2_4_eventsincountry.sql
@@ -2,6 +2,6 @@ SELECT
   c.country_name,
   count(e)
 FROM countries c
-  JOIN venues v ON v.country_code = c.country_code
-  JOIN events e ON e.venue_id = v.venue_id
+  LEFT JOIN venues v ON v.country_code = c.country_code
+  LEFT JOIN events e ON e.venue_id = v.venue_id
 GROUP BY c.country_name;
